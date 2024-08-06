485ff7efb6ccaa891379087062c65ae8206d1bd0
SeungJin051 <seungjin051@gmail.com>
2024-08-06 19:24:33 +0900

    상품 목록 컴포넌트화 및 components 폴더 소개

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,19 +1,18 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import ProductList from '@/components/ProductList';
 
 function ProductPage() {
-  const [products, setProducts] = useState();
-
-  useEffect(() => {
-    axios.get('http://localhost:4000/products"').then(res => {
-      res.data;
-    });
-  });
-  return <div style={{ color: 'blue' }}>Hello Next</div>;
+  return (
+    <div>
+      <h1>상품목록 페이지</h1>
+      <ProductList />
+    </div>
+  );
 }
+
 /**
  *  1. 상품 목록 - /
  *  2. 상품 상세 - /products/productId/1
  *  3. 장바구니 - /carts
  */
+
 export default ProductPage;
