9172ad19acffb7a67cb1d28ed4503cfc611cb769
SeungJin051 <seungjin051@gmail.com>
2024-08-07 22:12:55 +0900

    상품 상세 페이지 UI 스타일링

---
+++ components/ProductInfo.jsx
@@ -0,0 +1,18 @@
+import Image from 'next/image';
+import React from 'react';
+import styles from './ProductInfo.module.css';
+
+export default function ProductInfo({ productDetail }) {
+  return (
+    <div className={styles.container}>
+      <div>
+        <img src={productDetail.imageUrl} alt="" />
+      </div>
+      <div className={styles.description}>
+        <p>{productDetail.name}</p>
+        <p>{productDetail.price}$</p>
+        <button>장바구니에 담기</button>
+      </div>
+    </div>
+  );
+}
--- pages/products/[productId].jsx
+++ pages/products/[productId].jsx
@@ -1,16 +1,16 @@
 import { fetchProductInfo } from '@/api';
 import ProductHeader from '@/components/ProductHeader';
+import ProductInfo from '@/components/ProductInfo';
 import axios from 'axios';
 import React from 'react';
 
 // 상품 상세 정보 페이지 컴포넌트
-export default function ProductDetailPage({ message, productInfo }) {
+export default function ProductDetailPage({ productDetail }) {
   const headerTitle = '상품 상세 정보 페이지';
   return (
     <div>
       <ProductHeader title={headerTitle} />
-      <div>ProductDetailPage : {message}</div>
-      <p>ProductInfo : {productInfo.name}</p>
+      <ProductInfo productDetail={productDetail} />
     </div>
   );
 }
@@ -19,13 +19,12 @@ export async function getServerSideProps(context) {
   // /products/100
   console.log(`## productId : `, context.params.productId);
   const id = context.params.productId;
-  const res = await fetchProductInfo(id);
+  const { data } = await fetchProductInfo(id);
 
   // const res = await axios.get(`http://localhost:4000/products/${id}`);
   return {
     props: {
-      message: '서버에서 보내준 메세지',
-      productInfo: res.data,
+      productDetail: data,
     },
   };
 }
