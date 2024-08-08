3c77657d8d2af2cabe8faf28b8c1e2c8faee281e
SeungJin051 <seungjin051@gmail.com>
2024-08-08 10:06:28 +0900

    디스트럭처링

--- components/ProductInfo.jsx
+++ components/ProductInfo.jsx
@@ -6,9 +6,11 @@ import { createCartItem } from '@/api';
 
 export default function ProductInfo({ productDetail }) {
   const router = useRouter(); // useRouter를 이용한 2.
+  const { id, name, price, imageUrl } = productDetail; // 디스트럭처링
+
   const addCart = async () => {
     // 1. 장바구니에 담는 API 호출
-    const res = await createCartItem(productDetail.id, productDetail.name);
+    const res = await createCartItem({ id, name, price, imageUrl });
     console.log(res);
     alert('장바구니에 추가됨!');
     // 2. 장바구니 페이지로 이동
@@ -18,17 +20,11 @@ export default function ProductInfo({ productDetail }) {
   return (
     <div className={styles.container}>
       <div>
-        {/* <img src={productDetail.imageUrl} alt="" /> */}
-        <Image
-          src={productDetail.imageUrl}
-          alt={productDetail.name}
-          width={250}
-          height={250}
-        />
+        <Image src={imageUrl} alt={name} width={250} height={250} />
       </div>
       <div className={styles.description}>
-        <p>{productDetail.name}</p>
-        <p>{productDetail.price}$</p>
+        <p>{name}</p>
+        <p>{price}$</p>
         <button onClick={addCart}>장바구니에 담기</button>
       </div>
     </div>
