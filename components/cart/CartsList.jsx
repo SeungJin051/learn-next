37ddf18cddb579d18351fea7b79fb8076c95aa28
SeungJin051 <seungjin051@gmail.com>
2024-08-11 23:38:23 +0900

    장바구니 목록 데이터 갱신 기능 구현

--- components/cart/CartsList.jsx
+++ components/cart/CartsList.jsx
@@ -2,11 +2,13 @@ import Image from 'next/image';
 import React from 'react';
 import styles from './CartsList.module.css';
 import { removeCartItem } from '@/api';
+import { useRouter } from 'next/router';
 
 // map [1, 2, 3] -> [10, 20, 30] = 기존의 배열의 각각의 요소를 변화
 // reduce [1, 2, 3] -> 6 = 모두 더해서 각각의 값으로 뽑아줌
 
 export default function CartsList({ carts }) {
+  const router = useRouter();
   const totalPrice = carts.reduce((acc, cur) => {
     //  누적 + 현재값
     return acc + parseFloat(cur.price);
@@ -14,10 +16,12 @@ export default function CartsList({ carts }) {
 
   const removeCart = async id => {
     // 1. 삭제 API 호출
-    await removeCartItem(id);
-    alert('삭제가 되었습니다.');
+    const res = await removeCartItem(id);
+    alert(`${res.data.name} 삭제가 되었습니다.`);
 
     // 2. 상품 목록(배열) 갱신
+    router.replace(router.asPath); // 이동은 하지만 흔적을 남기지 않겠다.
+    // push -> 이동한다.
   };
 
   return (
